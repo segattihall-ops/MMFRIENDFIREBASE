94cb35f2ab266e33038d261e9f3d3432305e7433
Gleicimar Hall <segatti.hall@gmail.com>
2025-11-03 21:39:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6561894657-fd4f6",
+  "appId": "1:266154074197:web:333fdd219be827e30a74dc",
+  "apiKey": "AIzaSyDK8qy5WrTBCIZCGWpeElsDHXLYE2KqaLk",
+  "authDomain": "studio-6561894657-fd4f6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "266154074197"
+};
